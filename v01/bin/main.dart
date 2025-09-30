fbe58b2b1396cec292b5576de4e31ef423eaa9b2
kyledies <80546548+kyledies@users.noreply.github.com>
2025-09-30 12:06:35 +0200

    Veckouppgift
    Basic MVP v01

---
+++ v01/bin/main.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:v01/v01.dart' as v01;
+void main() {
+  print('Ange första talet:');
+  String? tal1 = stdin.readLineSync();
+  int numerictal1 = int.parse(tal1!);
+  print('Ange andra talet:');
+  String? tal2 = stdin.readLineSync();
+  int numerictal2 = int.parse(tal2!);
+  
+  print('Du valde $numerictal1 och $numerictal2');
+  print('Ange operation: +,-,*,/,**:');
+  String? operation = stdin.readLineSync();
+  if (operation == '+') {
+    print('$numerictal1 + $numerictal2 = ${v01.addition(numerictal1, numerictal2)}');
+  } else if (operation == '-') {
+    print('$numerictal1 - $numerictal2 = ${v01.subtraction(numerictal1, numerictal2)}');
+  } else if (operation == '*') {
+    print('$numerictal1 * $numerictal2 = ${v01.multiplication(numerictal1, numerictal2)}');
+  } else if (operation == '/') {
+    print('$numerictal1 / $numerictal2 = ${v01.division(numerictal1, numerictal2)}');
+  } else if (operation == '**') {
+    print('$numerictal1 upphoejt med $numerictal2 = ${v01.powerby(numerictal1, numerictal2)}');
+  } else {
+    print('Ogiltig operation');
+  }
+
+}
--- v01/bin/v01.dart
+++
@@ -1,5 +0,0 @@
-import 'package:v01/v01.dart' as v01;
-
-void main(List<String> arguments) {
-  print('Hello world: ${v01.calculate()}!');
-}
