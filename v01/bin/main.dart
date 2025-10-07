299847d43f80e32d0a59aafc5b409ae239f65778
kyledies <80546548+kyledies@users.noreply.github.com>
2025-10-07 10:31:34 +0200

    Rättad kod

--- v01/bin/main.dart
+++ v01/bin/main.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
 import 'package:v01/v01.dart' as v01;
 void main() {
+
   print('Ange första talet:');
   String? tal1 = stdin.readLineSync();
   int numerictal1 = int.parse(tal1!);
+  
   print('Ange andra talet:');
   String? tal2 = stdin.readLineSync();
+<<<<<<< Updated upstream
   int numerictal2 = int.parse(tal2!);
+=======
+  int numerictal2 = int.parse(tal2!);  
+>>>>>>> Stashed changes
   
   print('Du valde $numerictal1 och $numerictal2');
   print('Ange operation: +,-,*,/,**:');
